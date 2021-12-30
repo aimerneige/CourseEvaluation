c533fa343ab3f4596afdc2cbf2ced53a5f8e429c
Aimer Neige <aimer.neige.soft@gmail.com>
2021-12-30 17:25:06 +0800

    enable swagger

--- src/main/java/com/aimerneige/course_evaluation/CourseEvaluationApplication.java
+++ src/main/java/com/aimerneige/course_evaluation/CourseEvaluationApplication.java
@@ -3,11 +3,14 @@ package com.aimerneige.course_evaluation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CourseEvaluationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CourseEvaluationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CourseEvaluationApplication.class, args);
+    }
 
 }
