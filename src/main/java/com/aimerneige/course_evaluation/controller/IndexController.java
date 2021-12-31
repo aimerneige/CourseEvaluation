74474ddd46104f2e2c1c19198cf69c6a1c642b84
Aimer Neige <aimer.neige.soft@gmail.com>
2021-12-31 16:40:06 +0800

    update index controller

--- src/main/java/com/aimerneige/course_evaluation/controller/IndexController.java
+++ src/main/java/com/aimerneige/course_evaluation/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.aimerneige.course_evaluation.controller;
 
+import com.aimerneige.course_evaluation.response.Response;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("")
-    public String index() {
-        return "Hello World!";
+    public Response index() {
+        return Response.success("Hello World!");
     }
 }
