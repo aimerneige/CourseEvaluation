35c43908c0541c6b33c7144ffb49c8231c35863d
Aimer Neige <aimer.neige.soft@gmail.com>
2021-12-31 12:51:42 +0800

    update md5 utils

---
+++ src/main/java/com/aimerneige/course_evaluation/utils/HashUtils.java
@@ -0,0 +1,27 @@
+package com.aimerneige.course_evaluation.utils;
+
+import java.security.MessageDigest;
+
+public class HashUtils {
+    public static String md5(String str) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte b[] = md.digest();
+            int i;
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+            return buf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
