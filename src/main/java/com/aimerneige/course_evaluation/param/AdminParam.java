a8d3b5cbb92e22e765b45147e9a09e25c75db07f
Aimer Neige <aimer.neige.soft@gmail.com>
2021-12-31 09:44:21 +0800

    fix password missing

--- src/main/java/com/aimerneige/course_evaluation/param/AdminParam.java
+++ src/main/java/com/aimerneige/course_evaluation/param/AdminParam.java
@@ -3,6 +3,7 @@ package com.aimerneige.course_evaluation.param;
 public class AdminParam {
     private String name;
     private String username;
+    private String password;
 
     public String getName() {
         return this.name;
@@ -20,4 +21,11 @@ public class AdminParam {
         this.username = username;
     }
 
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
